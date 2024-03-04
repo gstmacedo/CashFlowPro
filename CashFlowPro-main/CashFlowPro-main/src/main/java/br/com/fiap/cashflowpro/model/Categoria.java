8720ef4724ca438b6069188f7dd0ec7fafd1aaa1
gstmacedo <132796665+gstmacedo@users.noreply.github.com>
2024-03-04 08:32:08 -0300

    Add files via upload

---
+++ CashFlowPro-main/CashFlowPro-main/src/main/java/br/com/fiap/cashflowpro/model/Categoria.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public record Categoria(Long id,String nome, String icone){
+    public Categoria(Long id,String nome, String icone){
+        var key = (id != null) ? id : Math.abs( new Random().nextLong());
+        this.id = key;
+        this.nome = nome;
+        this.icone = icone;
+    }
+}
